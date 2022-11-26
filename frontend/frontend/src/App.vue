e91b7b71af4cd7581a5864c4a4df997410afbde8
Austinmc41 <austinmc41@gmail.com>
2022-11-26 17:44:48 -0500

    adding input text fields for adding Vessels and testing out PrimeVue components

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -1,19 +1,49 @@
 
-<template>
-<h1>My name is Austin how are you</h1>
-</template>
-
-
 <script>
 import axios from 'axios'
+import {ref} from 'vue';
 
-export default {
-  mounted() {
-    axios.get('http://0.0.0.0:8000/api/vessels/').
-    then((response) => {
-      console.log(response.data.payload)
-    })
-  }
-}
+const text = ref();
+
+// export default {
+//   mounted() {
+//     axios.get('http://0.0.0.0:8000/api/vessels/').
+//     then((response) => {
+//       console.log(response.data.payload)
+//     })
+//   }
+// }
 </script>
+
+<template>
+  <div class="container">
+    <span class="p-float-label">
+      <InputText id="id" type="text" v-model="value" />
+      <label for="id">Vessel ID</label>
+    </span>
+    <span class="p-float-label">
+      <InputText id="time" type="text" v-model="value" />
+      <label for="time">Time Received</label>
+    </span>
+    <span class="p-float-label">
+      <InputText id="latitude" type="text" v-model="value" />
+      <label for="latitude">Latitude</label>
+    </span>
+    <span class="p-float-label">
+      <InputText id="longitude" type="text" v-model="value" />
+      <label for="longitude">Longitude</label>
+    </span>
+    <Button label="Add Vessel" @click="create"></Button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 20vh;
+}
+</style>
+
 
--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -1,11 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice'
+
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import 'primevue/resources/primevue.min.css' 
+import 'primeicons/primeicons.css'
 
 
-
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.component('Toast', Toast)
+app.use(createPinia());
+app.mount('#app');
