94ed701c184e16d7373f1c2a6ce7ccc458894380
Austinmc41 <austinmc41@gmail.com>
2022-11-27 20:02:58 -0500

    clean up script

--- csv_script/post_vessels.py
+++ csv_script/post_vessels.py
@@ -1,48 +1,19 @@
 import csv 
 import json 
 import requests
-import logging 
+
 
 url = 'http://0.0.0.0:8000/api/vessels/'
-# def post_vessels(csv_path, endpoint):
-json_array = []
-# open csv file to pull data from
+
 with open('./data-fs-exercise.csv', encoding='utf-8') as csvf:
     # open text file to write responses to 
     with open("file.txt", "a") as f: 
-        
         csvReader = csv.DictReader(csvf)
         headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
-        # row_no = 1
-
         for row in csvReader:
             # remove spaces from keys
             clean_row = {row.replace(" ", ""): v for row, v in row.items()}
-            # json_row = json.dumps(clean_row)
-            # json_array.append(json_row)
-
-
-
-    
-    
-
-    # print(json_array)
-    
-
-
-    # print(json_string)
-
-    # for data in json_array:
-    #     print(json.dumps(data))
-
-    # response = requests.post(url, data=json.dumps(json_array), headers=headers)
-
-    # print(response.text)
-
-
-
-
-
+            
             # stringify dict
             vessel_str = json.dumps(clean_row)
         
