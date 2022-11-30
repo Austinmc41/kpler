7b201b5610e99fcd988b0d35b8895208f48ef1c6
Austinmc41 <austinmc41@gmail.com>
2022-11-30 12:57:43 -0500

    finishing touches

--- csv_script/post_vessels.py
+++ csv_script/post_vessels.py
@@ -2,7 +2,7 @@ import csv
 import json 
 import requests
 
-
+print('This could take a minute...')
 url = 'http://0.0.0.0:8000/api/vessels/'
 
 with open('./data-fs-exercise.csv', encoding='utf-8') as csvf:
@@ -27,13 +27,11 @@ with open('./data-fs-exercise.csv', encoding='utf-8') as csvf:
         f.write(response.text)
         f.write("\n")
 
-        for row, subresponse in zip(all_rows, response):
-            subresponse = json.loads(subresponse)
-            
-            input()
 
-
-            # This solution clearly has a lot to be desired. While it currently uploads all of the data at once 
-            # It takes a bit to do so. To get rid of the data upload bottleneck, 
-            # a future solution would include bulk csv imports from the backend using a django management command for db seeding, 
-            # but currently there are not model level data validations and that would have to be created first. 
+        # This solution clearly has a lot to be desired. While it currently uploads all of the data at once 
+        # It takes a bit to do so. To get rid of the data upload bottleneck, 
+        # a future solution would include bulk csv imports from the backend using a django management command for db seeding, 
+        # but currently there are not model level data validations and that would have to be created first.
+        # To find errors cmd + f on mac and type errors. You can most likely narrow down to the exact row in the csv 
+        # file by searching for the dates of successful requests. Also, prettifying the text file is an option if 
+        # a using a tool that can handle large amounts of text. 
