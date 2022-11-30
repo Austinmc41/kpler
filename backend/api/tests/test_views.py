b6e4847159aef0bf607b1e76d44546e5b5d96b66
Austinmc41 <austinmc41@gmail.com>
2022-11-29 20:51:35 -0500

    adding get all vessels test coverage

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -4,7 +4,44 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from ..models import Vessel
 from ..serializers import VesselSerializer
+import dateutil
+from datetime import datetime
 
 
 # initialize the APIClient app
 client = Client()
+
+
+class GetAllVessels(TestCase):
+    """ Test module for GET all Vessels API """
+
+    def setUp(self):
+        Vessel.objects.create(
+            vessel_id=10,
+            received_time_utc= datetime(2020, 3, 4, 14, 57, 11, tzinfo=dateutil.tz.tz.tzutc()),
+            latitude= -9,
+            longitude= -19)
+        Vessel.objects.create(
+            vessel_id=11,
+            received_time_utc= datetime(2021, 3, 4, 14, 57, 11, tzinfo=dateutil.tz.tz.tzutc()),
+            latitude= -9,
+            longitude= -19)
+        Vessel.objects.create(
+            vessel_id=12,
+            received_time_utc= datetime(2022, 3, 4, 14, 57, 11, tzinfo=dateutil.tz.tz.tzutc()),
+            latitude= -9,
+            longitude= -19)
+        Vessel.objects.create(
+            vessel_id=13,
+            received_time_utc= datetime(2019, 3, 4, 14, 57, 11, tzinfo=dateutil.tz.tz.tzutc()),
+            latitude= -9,
+            longitude= -19)
+
+    def test_get_all_vessels(self):
+       # get API response
+        response = client.get(reverse('vessels'))
+        # get data from db
+        vessels = Vessel.objects.all()
+        serializer = VesselSerializer(vessels, many=True)
+        self.assertEqual(response.data['payload'], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
